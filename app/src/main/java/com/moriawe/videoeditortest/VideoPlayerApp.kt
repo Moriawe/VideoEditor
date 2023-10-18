1246251d55f35070ea0fea823d9b4954a033fd46
jennie.lindahl <jennie.lindahl@lynkco.com>
2023-10-18 10:11:21 +0200

    added dagger hilt modules

---
+++ app/src/main/java/com/moriawe/videoeditortest/VideoPlayerApp.kt
@@ -0,0 +1,7 @@
+package com.moriawe.videoeditortest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class VideoPlayerApp: Application()
--- app/src/main/java/com/moriawe/videoeditortest/editor/Player.kt
+++
@@ -1,9 +0,0 @@
-package com.moriawe.videoeditortest.editor
-
-import androidx.media3.exoplayer.ExoPlayer
-
-class Player {
-
-    val player = ExoPlayer.Builder(context).build()
-
-}
