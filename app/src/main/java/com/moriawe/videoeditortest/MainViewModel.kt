e027f4c990af78828b39825631ff7f1197f78b03
jennie.lindahl <jennie.lindahl@lynkco.com>
2023-10-20 08:55:49 +0200

    Revert "Wrong refactoring"
    This reverts commit 3f0866f1f8813afde7718a9dfff89a38ef1ff058.

--- app/src/main/java/com/moriawe/videoeditortest/MainViewModel.kt
+++ app/src/main/java/com/moriawe/videoeditortest/MainViewModel.kt
@@ -1,4 +1,71 @@
 package com.moriawe.videoeditortest
 
-class MainViewModel {
+import android.net.Uri
+import android.util.Log
+import androidx.core.net.toUri
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    val player: Player,
+    private val metaDataReader: MetaDataReader,
+    private val videoTrimmer: VideoTrimmer
+): ViewModel() {
+
+    private val TAG = "MainViewModel"
+    private val videoUris = savedStateHandle.getStateFlow("videoUris", emptyList<Uri>())
+
+    val videoItems = videoUris.map { uris ->
+        uris.map {uri ->
+            VideoItem(
+                contentUri = uri,
+                mediaItem = MediaItem.fromUri(uri),
+                name = metaDataReader.getMetaDataFromUri(uri)?.fileName ?: "No name"
+            )
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+
+    init {
+        player.prepare()
+    }
+
+    fun addVideoUri(uri: Uri) {
+        savedStateHandle["videoUris"] = videoUris.value + uri
+        player.addMediaItem(MediaItem.fromUri(uri))
+    }
+
+    fun playVideo(uri: Uri) {
+        player.setMediaItem(
+            videoItems.value.find { it.contentUri == uri}?.mediaItem ?: return
+        )
+    }
+
+    fun trimVideo() {
+        val uri = player.currentMediaItem?.mediaId?.toUri()
+        if (uri != null) {
+            player.addMediaItem(
+                videoTrimmer.trimVideo(uri, 1000, 10000)
+            )
+            Log.d(TAG,"trimVideo: video is cut")
+        } else {
+            Log.d(TAG,"trimVideo: No video found")
+            println("You need to provide a video")
+        }
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        player.release()
+    }
 }
